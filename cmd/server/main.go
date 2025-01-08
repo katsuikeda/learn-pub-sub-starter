4e5ba3e0c0cf9b0b1020aed6e951ef2048fd5531
katsuikeda <katsuyukiikeda142857@gmail.com>
2025-01-08 12:07:12 -0800

    Remove Dockerfile and implement JSON publishing functionality in the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -19,6 +21,23 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	publishCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Couldn't create channel: %v", err)
+	}
+
+	err = pubsub.PublishJSON(
+		publishCh,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		})
+	if err != nil {
+		log.Fatalf("Couldn't publish time: %v", err)
+	}
+	fmt.Println("Pause message sent!")
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
